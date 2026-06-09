4545c821b60fc75c9e2e76d698424b02525edc85
Minh <223230201+BlackBeard1720@users.noreply.github.com>
2026-06-09 15:24:38 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const pool = require('./db');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/products', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM products');
+  res.json(rows);
+});
+
+app.get('/products/search', async (req, res) => {
+  const keyword = req.query.keyword || "";
+  const [rows] = await pool.query('SELECT * FROM products WHERE name LIKE ?', [`%${keyword}%`]);
+  res.json(rows);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [id]);
+
+  if(rows.length === 0) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
+  res.json(rows[0]);
+});
+
+app.post('/products', async (req, res) => {
+  const { name, price, stock, description } = req.body;
+
+  if(!name || !price){
+    return res.status(400).json({
+      message: 'Name and price required',
+    });
+  }
+
+  const [result] = await pool.query('INSERT INTO products (name, price, stock, description) VALUES (?, ?, ?, ?)',
+    [name, price, stock || 0, description || null]
+  );
+
+  res.status(201).json({
+    message: 'Product created successfully',
+    id: result.insertId
+  });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const { name, price, stock, description } = req.body;
+
+  if (!name || !price || stock === undefined) {
+    return res.status(400).json({
+      message: 'Name, price and stock are required',
+    });
+  }
+
+  const [result] = await pool.query('UPDATE products SET name = ?, price = ?, stock = ?, description = ? WHERE id = ?',
+    [name, price, stock, description || null, id]
+  );
+
+  if (result.affectedRows === 0) {
+    return res.status(404).json({
+      message: 'Product not found',
+    });
+  }
+
+  res.json({
+    message: 'Product updated successfully',
+  });
+});
+
+app.patch('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const { name, stock, price, description } = req.body;
+  
+  const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [id]);
+
+  if (rows.length === 0) {
+    return res.status(404).json({
+      message: "Product not found"
+    });
+  }
+
+  const oldProduct = rows[0];
+
+  const newName = name !== undefined ? name : oldProduct.name;
+  const newPrice = price !== undefined ? price : oldProduct.price;
+  const newStock = stock !== undefined ? stock : oldProduct.stock;
+  const newDescription = description !== undefined ? description : oldProduct.description;
+
+  await pool.query(
+    'UPDATE products SET name = ?, price = ?, stock = ?, description = ? WHERE id = ?',
+    [newName, newPrice, newStock, newDescription, id]
+  );
+
+  res.json({
+    message: 'Product updated partially successfully',
+  });
+});
+
+app.delete('/products/:id', async(req, res) => {
+  const id = req.params.id;
+  const [result] = await pool.query(
+    'DELETE FROM products WHERE id = ?',
+    [id]
+  );
+
+  if(result.affectedRows === 0) {
+    return  res.status(404).json({
+      message: "Product not found",
+    });
+  }
+
+  res.json({
+    message: "Product deleleted successfully",
+  });
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
